66b51a4ac52d9c703a84928218627a12cd043ca8
AminMarayed <aminmarayed4@gmail.com>
2024-06-11 13:27:12 +0330

    change next config for image url

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "vidapub.com",
+        port: "",
+        pathname: "/wp-content/uploads/2021/05/**",
+      },
+    ],
+  },
+};
 
 export default nextConfig;
