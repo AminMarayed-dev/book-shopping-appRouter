ac349fc76922ebd3c54bebee8e53068baa5b361a
AminMarayed <aminmarayed4@gmail.com>
2024-06-13 17:13:14 +0330

    ref: change style better

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,15 +5,15 @@ import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
 import IconButton from "@mui/material/IconButton";
 import MenuIcon from "@mui/icons-material/Menu";
-import SearchIcon from "@mui/icons-material/Search";
 import MoreIcon from "@mui/icons-material/MoreVert";
 import InputBase from "@mui/material/InputBase";
 import PersonIcon from "@mui/icons-material/Person";
 import LocalMallIcon from "@mui/icons-material/LocalMall";
 import Image from "next/image";
-import { Drawer } from "@mui/material";
+import { Divider, Drawer, InputAdornment, TextField } from "@mui/material";
 import { useState } from "react";
 import DrawerList from "./home/DrawerList";
+import SearchIcon from "@mui/icons-material/Search";
 
 const StyledToolbar = styled(Toolbar)(({ theme }) => ({
   alignItems: "flex-start",
@@ -22,51 +22,11 @@ const StyledToolbar = styled(Toolbar)(({ theme }) => ({
   paddingBottom: theme.spacing(2),
   // Override media queries injected by theme.mixins.toolbar
   "@media all": {
-    minHeight: 128,
+    minHeight: 60,
   },
 }));
 
-const Search = styled("div")(({ theme }) => ({
-  position: "relative",
-  borderRadius: theme.shape.borderRadius,
-  backgroundColor: alpha(theme.palette.common.white, 0.15),
-  "&:hover": {
-    backgroundColor: alpha(theme.palette.common.white, 0.25),
-  },
-  // marginRight: theme.spacing(2),
-  marginLeft: 0,
-  width: "100%",
-  [theme.breakpoints.up("sm")]: {
-    marginLeft: theme.spacing(3),
-    width: "auto",
-  },
-  marginTop: "1rem",
-}));
 
-const SearchIconWrapper = styled("div")(({ theme }) => ({
-  padding: theme.spacing(0, 2),
-  height: "100%",
-  position: "absolute",
-  pointerEvents: "none",
-  display: "flex",
-  alignItems: "center",
-  justifyContent: "center",
-}));
-
-const StyledInputBase = styled(InputBase)(({ theme }) => ({
-  color: "inherit",
-  border: "2px solid rgba(0, 0, 0, .1)",
-  borderRadius: "5px",
-  "& .MuiInputBase-input": {
-    padding: theme.spacing(1, 1, 1, 0),
-    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
-    transition: theme.transitions.create("width"),
-    width: "100%",
-    [theme.breakpoints.up("md")]: {
-      width: "20ch",
-    },
-  },
-}));
 
 function Header() {
   const [open, setOpen] = useState(false);
@@ -76,7 +36,7 @@ function Header() {
   };
   return (
     <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="sticky">
+      <AppBar position="sticky" elevation={0}>
         <StyledToolbar>
           <IconButton
             size="large"
@@ -89,7 +49,7 @@ function Header() {
             <MenuIcon />
           </IconButton>
           <Drawer open={open} onClose={toggleDrawer(false)}>
-            <DrawerList toggleDrawer={toggleDrawer}/>
+            <DrawerList toggleDrawer={toggleDrawer} />
           </Drawer>
           <Box
             sx={{
@@ -107,17 +67,6 @@ function Header() {
               width={140}
               height={140}
             />
-            <Search sx={{ flexGrow: 1, alignSelf: "flex-end" }}>
-              <SearchIconWrapper>
-                <SearchIcon />
-              </SearchIconWrapper>
-              <StyledInputBase
-                placeholder="جستجوی محصولات"
-                inputProps={{
-                  "aria-label": "search",
-                }}
-              />
-            </Search>
           </Box>
 
           <IconButton
@@ -139,6 +88,23 @@ function Header() {
           </IconButton>
         </StyledToolbar>
       </AppBar>
+      <Divider />
+      <Box sx={{p:1}}>
+        <TextField
+          fullWidth
+          placeholder="جستجوی کتاب"
+          InputProps={{
+            startAdornment: (
+              <InputAdornment position="start">
+                <SearchIcon />
+              </InputAdornment>
+            ),
+          }}
+          variant="outlined"
+          color="secondary"
+        />
+      </Box>
+      <Divider/>
     </Box>
   );
 }
