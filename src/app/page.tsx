866c9d23e99acc36622e862e16709f5158563e9a
AminMarayed <aminmarayed4@gmail.com>
2024-06-13 17:12:46 +0330

    feat: add banner and intro component to home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
 
+import Banner from "@/components/home/Banner";
+import Introduction from "@/components/home/Introduction";
 import ProductWrapper from "@/components/home/ProductWrapper";
 
 export default function Home() {
   return (
     <main>
+      <Banner/>
+      <Introduction/>
       <ProductWrapper/>
     </main>
   );
