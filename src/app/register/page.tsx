8dd9e830cf88f6dac953d0c2842a6d5ad87e35c5
Tuba Hosseini <tubahosseini@gmail.com>
2024-06-16 21:19:27 +0330

    structure of register page added

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,6 +1,12 @@
 "use client";
-function Register() {
-  return <div>Register</div>;
-}
 
-export default Register;
+import { useSearchParams } from "next/navigation";
+import SignUp from "./SignUp";
+import SignIn from "./SignIn";
+
+export default function Register() {
+  const searchParams = useSearchParams();
+  const mode = searchParams.get("mode");
+
+  return <>{mode === "signup" ? <SignUp /> : <SignIn />}</>;
+}
